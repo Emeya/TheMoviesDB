51885cf7fce106a809f0c04e2a47688b630f3938
Emeya <emeyappdeveloper@hotmail.com>
2020-08-19 21:25:18 -0500

    Initial Commit

---
+++ TheMoviesDB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheMoviesDB
+//
+//  Created by Manuel Soberanis on 19/08/20.
+//  Copyright © 2020 Manuel Soberanis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
