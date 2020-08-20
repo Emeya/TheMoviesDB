add85ceb8526b8aad888c38f26957a4d3463014a
Emeya <emeyappdeveloper@hotmail.com>
2020-08-20 13:06:20 -0500

    Fixed desc view

--- TheMoviesDB/Views/Movie Description/MovieDescriptionView+TableView.swift
+++ TheMoviesDB/Views/Movie Description/MovieDescriptionView+TableView.swift
@@ -22,6 +22,7 @@ extension MovieDescriptionView : UITableViewDelegate, UITableViewDataSource{
         cell.runtimeValue.text = "\(movieDetail.runtime!) min"
         cell.releaseValue.text = self.date
         cell.calificacionValue.text = "\(movieDetail.vote_average!)"
+        //MARK: need ,
         cell.generosValue.text = genreArray.joined(separator: ", ")
         cell.desctipcionValue.text = movieDetail.overview
         return cell
